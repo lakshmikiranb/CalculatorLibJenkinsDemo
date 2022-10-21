9f3e7cd3435f43b8a249fb8d1ca6a4fffb04a860
lakshmikiranb <lakshmi2000kiran@gmail.com>
2022-10-21 10:53:12 +0530

    Calculator

---
+++ CalculatorLibJenkinsDemo/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibJenkinsDemo
+{
+   public class Calculator
+    {
+        public int Add(int i,int j)
+        { 
+            return i + j; 
+        }
+        public int Sub(int i,int j)
+        {
+            return i - j;
+        }
+        public int Mul(int i,int j)
+        {
+            return i * j;
+        }
+    }
+}
